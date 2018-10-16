6f8444d6a06acdd69f347ae8a3cdd99ce68d1f8c
Mingkun Wang <35738124+MKwang24@users.noreply.github.com>
2018-10-16 01:31:03 -0400

    Update FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,4 @@
-// Your solution to MIE250 Project 1 goes here
-// This is the "default package" hence there should be *no* line that starts with "package ...;"
-// Your solution to MIE250 Project 1 goes here
-// This is the "default package" hence there should be *no* line that starts with "package ...;"
+
 import java.io.*;
 
 public class FizzBuzz {
